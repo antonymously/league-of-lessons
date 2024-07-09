66ab660613591c2f065044bc051a92cad3105a9f
raf-madrigal <rfgmadrigal@gmail.com>
2024-07-09 04:02:18 -0400

    docs: edited ipynb instructions for TTS. set default voice in tts.py

--- league_of_lessons/tts/tts.py
+++ league_of_lessons/tts/tts.py
@@ -3,6 +3,14 @@ from pyht.client import TTSOptions
 import tempfile
 import wave
 import os
+import requests
+import json
+
+if not os.environ.get('PYHT_VOICE_NARRATOR', False):
+
+    # Set default Voice
+    os.environ['PYHT_VOICE_NARRATOR'] = 's3://voice-cloning-zero-shot/b3def996-302e-486f-a234-172fa0279f0e/anthonysaad/manifest.json'
+
 
 
 def text_to_speech(text_to_convert):
@@ -28,3 +36,20 @@ def text_to_speech(text_to_convert):
 
     return audio_file_path
 
+def get_list_of_voices(endpoint='v2'):
+    if endpoint=='v2':
+        
+        url = "https://api.play.ht/api/v2/voices"
+
+        headers = {
+            "accept": "application/json",
+            "AUTHORIZATION": os.environ['PYHT_SECRET'], 
+            "X-USER-ID": os.environ['PYHT_USER_ID'], 
+            
+        }
+
+        response = requests.get(url, headers=headers)
+        list_of_voices = response.json()
+    
+
+    return list_of_voices
